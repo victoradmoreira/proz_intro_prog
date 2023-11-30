9807407605ad854033a39ebfeb2d7dbab447d3fe
Victor <victoradmoreira@gmail.com>
2023-11-30 20:05:48 -0300

    concluído atividade

--- JavaScript_I/Desenvolvimento_5.1/script.js
+++ JavaScript_I/Desenvolvimento_5.1/script.js
@@ -1,8 +1,17 @@
-let titulo = document.getElementById("#titulo");
-titulo.innerText = "Inserir novo título";
+const h1 = document.createElement("h1")
+const a = document.createElement("a")
+const ul = document.createElement("ul")
+const ol = document.createElement("ol")
 
-console.log(titulo);
+h1.innerText = `novo título`
+a.innerText = `href="https://prozeducacao.com.br"`
+ul.innerHTML = `<li>item1</li> <li>item2</li> <li>item3</li>`
+ol.innerHTML = `<li> <a href="https://www.youtube.com">Youtube</a> 
+</li> <li> <a href="https://www.amazon.com.br">Amazon</a> 
+</li> <li> <a href="https://lichess.org/">Lichess</a> </li>`
 
-let link = document.getElementById("a")
-
-let lista = document.getElementById("ol")
+const body = document.querySelector('body')
+body.appendChild(h1)
+body.appendChild(a)
+body.appendChild(ul)
+body.appendChild(ol)
