c81b6a0d33a67a349ee66d417147efb461d122af
Victor Moreira <147779568+victoradmoreira@users.noreply.github.com>
2024-01-24 17:17:44 -0300

    Desenvolvimento_6-PostgreSQL.sql

--- BancoDados_I/BD_I-Desenvolvimento_6-PostgreSQL.sql
+++ BancoDados_I/BD_I-Desenvolvimento_6-PostgreSQL.sql
@@ -11,15 +11,15 @@ CREATE TABLE biblioteca (
   id SERIAL PRIMARY KEY NOT NULL);
 
 INSERT INTO aluno VALUES 
-	('João Carlos', 'jcarlos@gmail.com', 'Rua 13 de maio'),
+    ('João Carlos', 'jcarlos@gmail.com', 'Rua 13 de maio'),
     ('José Vitor', 'jvitor@gmail.com', 'Rua da Saudade'),
-    ('Paulo André', 'pandr@gmail.com', 'Rua do Sol')
+    ('Paulo André', 'pandr@gmail.com', 'Rua do Sol');
    	
 INSERT INTO biblioteca VALUES 
-	('Livro sobre a vida', 'Nicolau Aníbal', '13');
-    ('A busca por Fana', 'Amanda Nogueira', '11');
-    ('A Rua Desconhecida', 'Manuel Noel', '7');
-    ('Amor Esquecido', 'Manuel Noel', '7');
+    ('Livro sobre a vida', 'Nicolau Aníbal', '13'),
+    ('A busca por Fana', 'Amanda Nogueira', '11'),
+    ('A Rua Desconhecida', 'Manuel Noel', '7'),
+    ('Amor Esquecido', 'Manuel Noel', '7'),
     ('Labirinto', 'Rodrigo Fagundes', '5');
 
 
